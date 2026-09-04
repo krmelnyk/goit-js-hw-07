7ad50d7f0fdbf087c2a5c9b231433d79172ebf04
shwed <krmelnik@gmail.com>
2026-09-04 21:23:44 +0300

    Add login form styles and functionality with Montserrat font

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  if (!formData.email || !formData.password) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  console.log(formData);
+  event.currentTarget.reset();
+});
