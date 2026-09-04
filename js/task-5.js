09236767aa6468d928575e199b922754528ec6ab
shwed <krmelnik@gmail.com>
2026-09-04 21:26:58 +0300

    Add widget styles and Montserrat font to enhance UI

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+  colorValue.textContent = newColor;
+});
