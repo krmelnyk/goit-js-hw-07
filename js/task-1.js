93b2550a0ef382d83fbb1cb63f9fb4ce7554bdb0
shwed <krmelnik@gmail.com>
2026-09-04 20:47:43 +0300

    Add JavaScript functionality to log category details and fix spelling error in HTML

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categories = document.querySelectorAll('#categories .item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  const title = category.querySelector('h2');
+  const elements = category.querySelectorAll('ul li');
+
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Elements: ${elements.length}`);
+});
